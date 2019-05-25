ef803070525c8445f2b4ea636e783f76f187463f
Diego Bernal <diego91bernal@gmail.com>
2019-05-24 19:54:18 -0700

    feature: add form validation

--- root/src/site.js
+++ root/src/site.js
@@ -174,4 +174,80 @@ $(document).ready(() => {
             }
         });
     }
+
+    if ($("#contact-form").length > 0) {
+        var $contactForm = $("#contact-form");
+        var $formSubmitButton = $("button.btn-submit");
+        $contactForm.submit(function(e) {
+            e.preventDefault();
+
+            var $form = $(this);
+            if ($form[0].checkValidity()) {
+                var $cardContent = $contactForm.parent();
+                $.post($form.attr("action"), $form.serialize()).then(
+                    function success() {
+                        var successAlert = [
+                            '<div class="mt-3 alert alert-success alert-dismissible fade show" role="alert">',
+                            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">',
+                            '<span aria-hidden="true">&times;</span>',
+                            "</button>",
+                            "<strong>Success!</strong> We have received your message and will reply to it as soon as possible.",
+                            "</div>"
+                        ]
+                            .join("")
+                            .replace(/\s\s+/g, "");
+
+                        $cardContent.append(successAlert);
+
+                        $contactForm.trigger("reset");
+                    },
+                    function error() {
+                        var errorAlert = [
+                            '<div class="mt-3 alert alert-danger alert-dismissible fade show" role="alert">',
+                            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">',
+                            '<span aria-hidden="true">&times;</span>',
+                            "</button>",
+                            "<strong>Oops!</strong> An error occurred while sending your message. Please try again.",
+                            "</div>"
+                        ]
+                            .join("")
+                            .replace(/\s\s+/g, "");
+
+                        $cardContent.append(errorAlert);
+
+                        $contactForm.trigger("reset");
+
+                        $formSubmitButton.attr("disabled", true);
+                    }
+                );
+            }
+        });
+
+        var inputs = document.querySelectorAll("input, select, textarea, select");
+        inputs.forEach(function(input) {
+            var $input = $(input);
+            var $formGroup = $input.parents(".form-group");
+            var $feedback = $formGroup.find(".form-control-feedback");
+
+            input.addEventListener("input", function() {
+                if (input.validity.valid) {
+                    $formGroup.removeClass("has-danger");
+                } else {
+                    $formGroup.addClass("has-danger");
+                    if (input.validity.typeMismatch && $input.attr("name") == "email") {
+                        $feedback.html("Please provide a valid email");
+                    }
+                    if (input.validity.valueMissing && $input.attr("name") == "email") {
+                        $feedback.html("Email is required");
+                    }
+                }
+
+                if ($contactForm[0].checkValidity()) {
+                    $formSubmitButton.removeAttr("disabled");
+                } else {
+                    $formSubmitButton.attr("disabled", true);
+                }
+            });
+        });
+    }
 });
