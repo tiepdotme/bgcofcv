d20dd5f686a673b69e9974caa52523d9f3aa010c
Diego Bernal <diego91bernal@gmail.com>
2020-04-06 20:00:04 -0700

    add mecca virtual schedule

--- root/jekyll/_our-clubs/mecca.md
+++ root/jekyll/_our-clubs/mecca.md
@@ -14,7 +14,62 @@ summerHours: "7:30am - 5:30pm"
 image: assets/images/mecca-club.jpg
 calendar: /assets/images/october-calendar-2019-mecca.pdf
 zoomUrl: https://us04web.zoom.us/j/5520528777
-isVirtualLive: false
+isVirtualLive: true
+virtualSchedule:
+    - day: "Mon"
+      dayNumber: 1
+      start: null
+      end: null
+      isOff: true
+      slots: null
+    - day: "Tue"
+      dayNumber: 2
+      start: 10
+      end: 16
+      isOff: false
+      slots:
+          - time: "10:00am - 10:55am"
+            subject: "Healthy Habits with Erika"
+          - time: "11:00am - 11:55am"
+            subject: "Arts & Crafts with Jervoa and Leslie"
+          - time: "12:00pm - 12:55pm"
+            subject: "Tech & S.T.E.M with Veronica"
+          - time: "1:00pm - 1:55pm"
+            subject: "Education & Tutoring & Homework Help with Albert"
+          - time: "2:00pm - 2:55pm"
+            subject: "Boxing with Shaggy & Ray Ray"
+          - time: "3:00pm - 3:55pm"
+            subject: "Torch club with Sofia"
+    - day: "Wed"
+      dayNumber: 3
+      start: null
+      end: null
+      isOff: true
+      slots: null
+    - day: "Thu"
+      dayNumber: 4
+      start: 10
+      end: 16
+      isOff: false
+      slots:
+          - time: "10:00am - 10:55am"
+            subject: "Healthy Habits with Erika"
+          - time: "11:00am - 11:55am"
+            subject: "Arts & Crafts with Jervoa and Leslie"
+          - time: "12:00pm - 12:55pm"
+            subject: "Tech & S.T.E.M with Veronica"
+          - time: "1:00pm - 1:55pm"
+            subject: "Education & Tutoring & Homework Help with Albert"
+          - time: "2:00pm - 2:55pm"
+            subject: "Boxing with Shaggy & Ray Ray"
+          - time: "3:00pm - 3:55pm"
+            subject: "Palabra with Victor Ronnie Ray  "
+    - day: "Fri"
+      dayNumber: 5
+      start: null
+      end: null
+      isOff: true
+      slots: null
 ---
 
 A state-of-the-art facility where members can enjoy games, sports activities, technology, and more! Our Joel E. Smilow Clubhouse currently serves 1,000 youth in Mecca and surrounding cities. In a partnership with the County of Riverside, this Club opened in 2011.
--- root/src/site.js
+++ root/src/site.js
@@ -27,7 +27,7 @@ $(document).ready(() => {
                             "rgba(255, 206, 86, 1)",
                             "rgba(75, 192, 192, 1)",
                             "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)"
+                            "rgba(255, 159, 64, 1)",
                         ],
                         borderColor: [
                             "rgba(255,99,132,1)",
@@ -35,30 +35,30 @@ $(document).ready(() => {
                             "rgba(255, 206, 86, 1)",
                             "rgba(75, 192, 192, 1)",
                             "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)"
+                            "rgba(255, 159, 64, 1)",
                         ],
-                        borderWidth: 1
-                    }
-                ]
+                        borderWidth: 1,
+                    },
+                ],
             },
             options: {
                 scales: {
                     yAxes: [
                         {
                             ticks: {
-                                beginAtZero: true
-                            }
-                        }
-                    ]
+                                beginAtZero: true,
+                            },
+                        },
+                    ],
                 },
                 title: {
                     text: "Membership Growth",
-                    display: true
+                    display: true,
                 },
                 legend: {
-                    display: false
-                }
-            }
+                    display: false,
+                },
+            },
         });
 
         const diversityChartCtx = $("#diversity");
@@ -66,23 +66,23 @@ $(document).ready(() => {
             options: {
                 title: {
                     text: "Ethnicity",
-                    display: true
+                    display: true,
                 },
                 legend: {
                     labels: {
                         padding: 5,
-                        fontSize: 11
-                    }
+                        fontSize: 11,
+                    },
                 },
                 layout: {
-                    padding: 5
+                    padding: 5,
                 },
                 plugins: {
                     labels: {
                         render: "percentage",
-                        overlap: false
-                    }
-                }
+                        overlap: false,
+                    },
+                },
             },
             type: "pie",
             data: {
@@ -91,7 +91,7 @@ $(document).ready(() => {
                     "Other",
                     "African-American",
                     "Caucasian",
-                    "Multi-Racial"
+                    "Multi-Racial",
                 ],
                 datasets: [
                     {
@@ -102,19 +102,19 @@ $(document).ready(() => {
                             "rgba(54, 162, 235, 1)",
                             "rgba(255, 206, 86, 1)",
                             "rgba(75, 192, 192, 1)",
-                            "rgba(255, 159, 64, 1)"
+                            "rgba(255, 159, 64, 1)",
                         ],
                         borderColor: [
                             "rgba(255,99,132,1)",
                             "rgba(54, 162, 235, 1)",
                             "rgba(255, 206, 86, 1)",
                             "rgba(75, 192, 192, 1)",
-                            "rgba(255, 159, 64, 1)"
+                            "rgba(255, 159, 64, 1)",
                         ],
-                        borderWidth: 1
-                    }
-                ]
-            }
+                        borderWidth: 1,
+                    },
+                ],
+            },
         });
 
         const agesChartCtx = $("#ages");
@@ -122,17 +122,17 @@ $(document).ready(() => {
             options: {
                 title: {
                     text: "Age Groups",
-                    display: true
+                    display: true,
                 },
                 layout: {
-                    padding: 5
+                    padding: 5,
                 },
                 plugins: {
                     labels: {
                         render: "percentage",
-                        overlap: false
-                    }
-                }
+                        overlap: false,
+                    },
+                },
             },
             type: "pie",
             data: {
@@ -143,17 +143,17 @@ $(document).ready(() => {
                         backgroundColor: [
                             "rgba(255, 99, 132, 1)",
                             "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)"
+                            "rgba(255, 206, 86, 1)",
                         ],
                         borderColor: [
                             "rgba(255,99,132,1)",
                             "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)"
+                            "rgba(255, 206, 86, 1)",
                         ],
-                        borderWidth: 1
-                    }
-                ]
-            }
+                        borderWidth: 1,
+                    },
+                ],
+            },
         });
 
         const genderChartCtx = $("#gender");
@@ -161,17 +161,17 @@ $(document).ready(() => {
             options: {
                 title: {
                     text: "Gender",
-                    display: true
+                    display: true,
                 },
                 layout: {
-                    padding: 5
+                    padding: 5,
                 },
                 plugins: {
                     labels: {
                         render: "percentage",
-                        overlap: false
-                    }
-                }
+                        overlap: false,
+                    },
+                },
             },
             type: "pie",
             data: {
@@ -182,16 +182,16 @@ $(document).ready(() => {
                         data: [56, 44],
                         backgroundColor: [
                             "rgba(255, 99, 132, 1)",
-                            "rgba(54, 162, 235, 1)"
+                            "rgba(54, 162, 235, 1)",
                         ],
                         borderColor: [
                             "rgba(255,99,132,1)",
-                            "rgba(54, 162, 235, 1)"
+                            "rgba(54, 162, 235, 1)",
                         ],
-                        borderWidth: 1
-                    }
-                ]
-            }
+                        borderWidth: 1,
+                    },
+                ],
+            },
         });
     }
 
@@ -200,7 +200,7 @@ $(document).ready(() => {
         var $formSubmitButton = $("button.btn-submit");
         var $cardContent = $contactForm.parent();
 
-        $contactForm.submit(function(e) {
+        $contactForm.submit(function (e) {
             e.preventDefault();
 
             var $recaptcha = $("#g-recaptcha-response");
@@ -209,7 +209,7 @@ $(document).ready(() => {
                 var errorAlert = [
                     '<p class="recaptcha-error text-danger text-center">',
                     "Please show that you're not a robot",
-                    "</p>"
+                    "</p>",
                 ]
                     .join("")
                     .replace(/\s\s+/g, "");
@@ -220,7 +220,7 @@ $(document).ready(() => {
             var $form = $(this);
             if ($form[0].checkValidity()) {
                 $.post($form.attr("action"), $form.serialize())
-                    .then(function() {
+                    .then(function () {
                         $(".recaptcha-error").remove();
 
                         var successAlert = [
@@ -229,7 +229,7 @@ $(document).ready(() => {
                             '<span aria-hidden="true">&times;</span>',
                             "</button>",
                             "<strong>Success!</strong> We have received your message and will reply to it as soon as possible.",
-                            "</div>"
+                            "</div>",
                         ]
                             .join("")
                             .replace(/\s\s+/g, "");
@@ -238,7 +238,7 @@ $(document).ready(() => {
 
                         $contactForm.trigger("reset");
                     })
-                    .catch(function() {
+                    .catch(function () {
                         $(".recaptcha-error").remove();
 
                         var errorAlert = [
@@ -247,7 +247,7 @@ $(document).ready(() => {
                             '<span aria-hidden="true">&times;</span>',
                             "</button>",
                             "<strong>Oops!</strong> An error occurred while sending your message. Please try again.",
-                            "</div>"
+                            "</div>",
                         ]
                             .join("")
                             .replace(/\s\s+/g, "");
@@ -262,12 +262,12 @@ $(document).ready(() => {
         });
 
         var inputs = document.querySelectorAll("input, textarea");
-        inputs.forEach(function(input) {
+        inputs.forEach(function (input) {
             var $input = $(input);
             var $formGroup = $input.parents(".form-group");
             var $feedback = $formGroup.find(".form-control-feedback");
 
-            input.addEventListener("input", function() {
+            input.addEventListener("input", function () {
                 if (input.validity.valid) {
                     $formGroup.removeClass("has-danger");
                 } else {
@@ -297,7 +297,7 @@ $(document).ready(() => {
 
     if ($("#read-more-03-20-2020").length > 0) {
         const readMoreLink = $("#read-more-03-20-2020");
-        readMoreLink.click(function() {
+        readMoreLink.click(function () {
             $(this).text(
                 // inverted logic
                 $(this).attr("aria-expanded") === "true"
@@ -308,7 +308,7 @@ $(document).ready(() => {
     }
 
     if ($("#virtual-club-page").length > 0) {
-        import(/* webpackChunkName: "date-fns" */ "date-fns").then(function(
+        import(/* webpackChunkName: "date-fns" */ "date-fns").then(function (
             module
         ) {
             const {
@@ -316,7 +316,8 @@ $(document).ready(() => {
                 startOfToday,
                 addHours,
                 getDay,
-                format
+                format,
+                addMinutes,
             } = module;
 
             const currentDayNumber = getDay(new Date());
@@ -328,19 +329,15 @@ $(document).ready(() => {
                     .addClass("active")
                     .attr("aria-selected", true);
 
-                $(".tab-pane")
-                    .first()
-                    .addClass("active show");
+                $(".tab-pane").first().addClass("active show");
 
-                $(".live-indicator").each(function() {
+                $(".live-indicator").each(function () {
                     $(this)
                         .find(".is-not-live")
                         .html("Join us weekdays. Find our schedule below")
                         .removeClass("d-none");
 
-                    $(this)
-                        .find(".is-live")
-                        .remove();
+                    $(this).find(".is-live").remove();
                 });
 
                 return;
@@ -363,12 +360,12 @@ $(document).ready(() => {
             const liveStart = addHours(startOfToday(), liveStartToday);
             const liveEnd = addHours(startOfToday(), liveEndToday);
             const isCurrentlyLive = isWithinInterval(now, {
-                start: liveStart,
-                end: liveEnd
+                start: addMinutes(liveStart, -15),
+                end: liveEnd,
             });
 
             if (isCurrentlyLive) {
-                $(".live-indicator").each(function() {
+                $(".live-indicator").each(function () {
                     $(this)
                         .find(".is-live")
                         .removeClass("d-none")
@@ -383,19 +380,13 @@ $(document).ready(() => {
                             )} to ${format(liveEnd, "hh:mmaaaa")}`
                         );
 
-                    $(this)
-                        .find(".is-not-live")
-                        .remove();
+                    $(this).find(".is-not-live").remove();
                 });
             } else {
-                $(".live-indicator").each(function() {
-                    $(this)
-                        .find(".is-not-live")
-                        .removeClass("d-none");
+                $(".live-indicator").each(function () {
+                    $(this).find(".is-not-live").removeClass("d-none");
 
-                    $(this)
-                        .find(".is-live")
-                        .remove();
+                    $(this).find(".is-live").remove();
                 });
             }
         });
